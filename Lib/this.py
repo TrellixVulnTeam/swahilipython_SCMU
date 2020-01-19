84575be8d2023826c91003e66e11feab2553af20
Kingsley Robertovich <kingsley.ndiewo@gini.tech>
2020-01-20 00:26:35 +0300

    Fix for poor replacement
    Minor fix

--- Lib/this.py
+++ Lib/this.py
@@ -12,12 +12,12 @@ Nygubhtu cenpgvpnyvgl orngf chevgl.
 Reebef fubhyq arire cnff fvyragyl.
 Hayrff rkcyvpvgyl fvyraprq.
 Va gur snpr bs nzovthvgl, ershfr gur grzcgngvba gb thrff.
-Gurer fubhyq ama bar-- naq cersrenoyl bayl bar --boivbhf jnl gb qb vg.
-Nygubhtu gung jnl znl abg ama boivbhf ng svefg hayrff lbh'er Qhgpu.
+Gurer fubhyq or bar-- naq cersrenoyl bayl bar --boivbhf jnl gb qb vg.
+Nygubhtu gung jnl znl abg or boivbhf ng svefg hayrff lbh'er Qhgpu.
 Abj vf orggre guna arire.
 Nygubhtu arire vf bsgra orggre guna *evtug* abj.
 Vs gur vzcyrzragngvba vf uneq gb rkcynva, vg'f n onq vqrn.
-Vs gur vzcyrzragngvba vf rnfl gb rkcynva, vg znl ama n tbbq vqrn.
+Vs gur vzcyrzragngvba vf rnfl gb rkcynva, vg znl or n tbbq vqrn.
 Anzrfcnprf ner bar ubaxvat terng vqrn -- yrg'f qb zber bs gubfr!"""
 
 d = {}
